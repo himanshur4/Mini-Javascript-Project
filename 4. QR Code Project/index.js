ad4a444876e3a18654a2384d32585a0a99cc95cc
Himanshu Raj <hraj808404@gmail.com>
2025-03-18 15:12:09 +0530

    Blog Project Added and previous project updated

--- 4. QR Code Project/index.js
+++ 4. QR Code Project/index.js
@@ -1,14 +1,9 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import inquirer from 'inquirer';
 import qr from "qr-image";
 import fs from "fs";
 inquirer
     .prompt([
-        /* Pass your questions in here */
+       
         {
             message:"Type in your url",
             name:"URL",
@@ -16,7 +11,6 @@ inquirer
         },
     ])
     .then((answers) => {
-        // Use user feedback for... whatever!!
         const url=answers.URL;
         var qr_svg = qr.image(url, { type: 'png' });
         qr_svg.pipe(fs.createWriteStream('qr-img.png'));
@@ -30,9 +24,9 @@ inquirer
     })
     .catch((error) => {
         if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
+            
          
         } else {
-            // Something else went wrong
+            
         }
     });
--- 5. Secrets Project 1/index.js
+++ 5. Secrets Project 1/index.js
@@ -6,12 +6,6 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
-// HINTs: Use the axios documentation as well as the video lesson to help you.
-// https://axios-http.com/docs/post_example
-// Use the Secrets API documentation to figure out what each route expects and how to work with it.
-// https://secrets-api.appbrewery.com/
-
-//TODO 1: Add your own bearer token from the previous lesson.
 const yourBearerToken = "c7564936-2348-4a0e-b0f1-6d8028d7eabd";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
@@ -34,7 +28,6 @@ app.post("/get-secret", async (req, res) => {
 });
 
 app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
   const searchId=req.body.id;
   try{
     const result=await axios.post(API_URL+"/secrets",req.body,config);
@@ -56,12 +49,10 @@ app.post("/put-secret", async (req, res) => {
   }catch(error){
     res.render("index.ejs",{content:JSON.stringify(error.response.data)});
   }
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
   try{
     const result=await axios.patch(API_URL+"/secrets/"+searchId,req.body,config);
     res.render("index.ejs",{content:JSON.stringify(result.data)});
@@ -74,8 +65,6 @@ app.post("/patch-secret", async (req, res) => {
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
-
-  // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
   try{
     const result=await axios.delete(API_URL+"/secrets/"+searchId,config);
     res.render("index.ejs",{content:JSON.stringify(result.data)});
